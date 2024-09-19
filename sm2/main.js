78a8059e276820d6b1aae27996ca9ed5a0ab06e4
unknown <vasina-alina05@mail.ru>
2024-09-19 11:16:56 +0300

    test

---
+++ sm2/main.js
@@ -0,0 +1,18 @@
+
+let name = prompt("Как тебя зовут?", '');
+alert (`ты ${name}`);
+
+
+let age = prompt('Сколько тебе лет?', 1);
+if (age < 20) {
+    alert ("Тебе меньше 20")
+}
+else {
+    for (let i=0; i<5; i++){
+    alert ("Привет")}
+}
+
+
+let js = confirm("Хочешь учить js?");
+if (js) {
+alert ("Супер")};
